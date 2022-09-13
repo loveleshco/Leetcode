9b343923035eeb07f5568c27ba34bff0d3409339
Lovelesh <loveleshcolaco.12@gmail.com>
2022-09-13 11:22:46 -0400

    Time: 1 ms (99.65%), Space: 40.1 MB (97.97%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        if(s.length() < 2)
+        {
+            return false;
+        }
+        
+        Stack<Character> st = new Stack<Character>();
+        for(int i= 0; i<s.length(); i++){
+            if(s.charAt(i) == '(')
+            {
+                st.push(')');
+                
+            }
+            else if(s.charAt(i) == '{')
+            {
+                st.push('}');
+                
+            }
+            else if(s.charAt(i) == '[')
+            {
+                st.push(']');
+                
+            }
+            else if(st.isEmpty() || st.pop() != s.charAt(i))
+            {
+                return false;
+            }
+            
+    }
+        return st.isEmpty();
+   
+}
+}
